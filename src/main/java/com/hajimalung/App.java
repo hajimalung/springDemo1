1f62533a5e8a966a13db0620e457adf38943ea4f
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-12-26 14:05:21 +0530

    adding spring context dep, using context to get object of alien

--- src/main/java/com/hajimalung/App.java
+++ src/main/java/com/hajimalung/App.java
@@ -1,5 +1,8 @@
 package com.hajimalung;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -10,7 +13,8 @@ public class App
     {
         System.out.println( "Hello World!" );
 
-        Alien obj = new Alien();
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        Alien obj = (Alien) context.getBean("alien");
         obj.code();
     }
 }
