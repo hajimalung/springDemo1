12387271117b3c4c0fb7053151a8a266768f3b4f
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2025-01-31 17:14:00 +0530

    vs code imported

--- src/main/java/com/hajimalung/App.java
+++ src/main/java/com/hajimalung/App.java
@@ -7,21 +7,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
         Alien obj = (Alien) context.getBean("alien");
         obj.name = "Hajimalung";
 
-//        Alien obj2 = (Alien) context.getBean("alien");
-//        obj2.name = "Daanish";
+        // Alien obj2 = (Alien) context.getBean("alien");
+        // obj2.name = "Daanish";
 
         obj.code();
-//        obj2.code();
+        // obj2.code();
     }
 }
